903e41e37af46e1c3218bf0c234955cbf8df4c62
Manav Misra <manavm@visionwebsoft.com>
2020-11-18 13:24:46 -0600

    :sparkles: Show/hide menu

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const dropdown = document.querySelector("#dropdown");
+
+document.querySelector("#toggle-nav").addEventListener("click", () => {
+  dropdown.classList.replace("is-hidden", "is-shown") ||
+    dropdown.classList.replace("is-shown", "is-hidden");
+});
