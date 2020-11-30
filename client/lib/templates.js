cb32a71e6507a5d4d9a4f37fdf2a71fc703df6ff
Manav Misra <manavm@visionwebsoft.com>
2020-11-30 15:32:36 -0600

    Show language with color

--- client/lib/templates.js
+++ client/lib/templates.js
@@ -2,6 +2,7 @@ export default {
   desc: document.querySelector("#template-repo-desc").content,
   descBtn: document.querySelector("#template-repo-desc-btn").content,
   forked: document.querySelector("#template-repo-forked").content,
+  language: document.querySelector("#template-repo-language").content,
   profileContent: document.querySelector("#template-profile").content,
   publicCount: document.querySelector("#template-count").content,
   repo: document.querySelector("#template-repo").content,
